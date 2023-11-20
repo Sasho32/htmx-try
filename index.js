9ef229306dcf78b477067998600b352007a157d1
Aleksandar Aleksandrov <aaleksandrov@beyonddata.bg>
2023-11-20 12:46:39 +0200

    deleted the js

--- index.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('button');
-const resultSection = document.querySelector('section#result');
-
-button.addEventListener('click', async e => {
-    const response = await fetch('http://127.0.0.1:3001/test');
-    const receivedHTML = await response.text();
-
-    resultSection.innerHTML = receivedHTML;
-});
